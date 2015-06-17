7dce0b20b24fab43def13d7a51e8dec23623246f
wangsen <578595193@qq.com>
2015-06-17 16:18:55 +0800

    按钮拓展类 添加按钮点击

--- IOS_ItdFoundation/FirstViewController.swift
+++ IOS_ItdFoundation/FirstViewController.swift
@@ -12,15 +12,25 @@ class FirstViewController: ItdViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.lightGrayColor()
         
-        var view:UIView = UIView(frame: CGRectMake(100, 100, 100, 100))
+        var view:UIView = UIView(frame: CGRectMake(10, 100, 30, 30))
         view.backgroundColor = UIColor.itd_colorWithHexInteger(0xff0000, alpha: 0.5)
         self.view.addSubview(view)
         
-        var textField:UITextField = UITextField(frame: CGRectMake(10, 250, 150, 50));
+        var textField:UITextField = UITextField(frame: CGRectMake(50, 100, 90, 30));
         textField.backgroundColor = UIColor.redColor()
-        textField.itd_limitTextLength(10)
+        textField.itd_limitTextLength(4)
         self.view.addSubview(textField)
+        
+        
+        var btn:UIButton = UIButton(frame: CGRectMake(textField.right_x() + 5, textField.top_y(), 50, 30))
+        btn.setTitle("按钮", forState: UIControlState.Normal)
+        btn.itd_clickDoBlock({ () -> () in
+            btn.setTitleColor(UIColor.redColor(), forState: UIControlState.Normal)
+            self.view.backgroundColor = UIColor.purpleColor()
+        })
+        self.view.addSubview(btn)
     }
 
     override func didReceiveMemoryWarning() {
--- ItdBaseUIKit/Extention/UIButton_Ext.swift
+++ ItdBaseUIKit/Extention/UIButton_Ext.swift
@@ -8,12 +8,41 @@
 
 import UIKit
 
-//extension UIButton{
-//    
-//    
-//    func _buttonClickDo_itd() -> Void{
-//        blockClick:VoidBlock
-//    }
-//    
-//    func itd_clickEvent_andDoBlock(event:UIControlEvents, doClosure())
-//}
+typealias VoidBlock = () -> () //或者 () -> Void
+
+extension UIButton{
+    
+    func _buttonClickDo_itd() -> Void{
+        if let cl = objc_getAssociatedObject(self, &(AssociatedKeys.overViewKeyButtonClick)) as? VoidClosureWrapper {
+            if let inDoClosure = cl.closure{
+                inDoClosure()
+            }
+        }
+    }
+    
+    func itd_clickDoBlock(doBlock:VoidBlock){
+        
+        objc_setAssociatedObject(
+            self,
+            &(AssociatedKeys.overViewKeyButtonClick),
+            VoidClosureWrapper(closure: doBlock),
+            UInt(OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+        )
+        
+        self.addTarget(self, action: "_buttonClickDo_itd", forControlEvents: UIControlEvents.TouchUpInside)
+    }
+    
+    
+    private class VoidClosureWrapper {
+        var closure: (() -> Void)?
+        
+        init(closure: (() -> Void)?) {
+            self.closure = closure
+        }
+    }
+    
+    
+    private struct AssociatedKeys {
+        static var overViewKeyButtonClick = "koverViewKeyButtonClick"
+    }
+}
