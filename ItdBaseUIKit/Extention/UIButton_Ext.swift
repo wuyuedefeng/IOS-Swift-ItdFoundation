258cd2ac2bc8d63ee9615961ccd757e67f98fa76
wangsen <578595193@qq.com>
2015-06-17 10:12:42 +0800

    定义在内部定义的函数 遵循JavaScript标准 不建议外部调用的方法前面加下划线

---
+++ ItdBaseUIKit/Extention/UIButton_Ext.swift
@@ -0,0 +1,13 @@
+//
+//  UIButton_Ext.swift
+//  IOS_ItdFoundation
+//
+//  Created by TX-009 on 15/6/17.
+//  Copyright (c) 2015年 TX-009. All rights reserved.
+//
+
+import UIKit
+
+//extension UIButton{
+//    func itd_clickEventAndDoBlock(event:UIControlEvents, )
+//}
--- ItdBaseUIKit/Extention/UITextField_Ext.swift
+++ ItdBaseUIKit/Extention/UITextField_Ext.swift
@@ -22,23 +22,23 @@ extension UITextField {
             UInt(OBJC_ASSOCIATION_RETAIN_NONATOMIC)
         )
         
-        self.addTarget(self, action: "itd_textFieldTextLengthLimit", forControlEvents: UIControlEvents.EditingChanged)
+        self.addTarget(self, action: "_itd_textFieldTextLengthLimit", forControlEvents: UIControlEvents.EditingChanged)
         
     }
     
-    public func itd_textFieldTextLengthLimit() ->Void{
+    func _itd_textFieldTextLengthLimit() ->Void{
 
-    let lengthNumber:NSString = objc_getAssociatedObject(self, &(AssociatedKeys.kLimitTextLengthKey)) as! NSString
+        let lengthNumber:NSString = objc_getAssociatedObject(self, &(AssociatedKeys.kLimitTextLengthKey)) as! NSString
 
 
-    var length:Int = lengthNumber.integerValue
-    var string:NSString = self.text
+        var length:Int = lengthNumber.integerValue
+        var string:NSString = self.text
 
-    if Int(string.length) > length{
-        var str = string.lowercaseString as String
-        var len:String.Index = advance(str.startIndex, length)
-        self.text = self.text.substringToIndex(len)
-    }
+        if Int(string.length) > length{
+            var str = string.lowercaseString as String
+            var len:String.Index = advance(str.startIndex, length)
+            self.text = self.text.substringToIndex(len)
+        }
 
     }
     
